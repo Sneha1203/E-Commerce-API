394124e71e9c50c9f9a49360ffcc1e71c3485498
Sneha1203 <78474043+Sneha1203@users.noreply.github.com>
2022-06-21 23:07:04 +0530

    getAllUsers and getSingleUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,20 @@
+const User = require('../models/User')
+const {StatusCodes} = require('http-status-codes')
+const CustomError = require('../errors')
+
 const getAllUsers = async(request, response) => {
-    response.send('get all users')
+    // response.send('get all users')
+    const users = await User.find({role: 'user'}).select('-password')
+    response.status(StatusCodes.OK).json({users})
 }
 
 const getSingleUser = async(request, response) => {
-    response.send('get single user')
+    // response.send('get single user')
+    const user = await User.findOne({_id: request.params.id}).select('-password')
+    if(!user) {
+        throw new CustomError.NotFoundError(`no user found with id: ${request.params.id}`)
+    }
+    response.status(StatusCodes.OK).json({user})
 }
 
 const showCurrentUser = async(request, response) => {
